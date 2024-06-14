64bbcc4036986c70de8e45fac7e9e3040accfd50
Alejandra De Luna <aledelunap@gmail.com>
2024-06-14 13:48:48 -0400

    refactor: dataset config

--- src/toy_data/data.rs
+++ src/toy_data/data.rs
@@ -11,6 +11,76 @@ use ndarray_rand::{
     rand_distr::{Distribution, Normal},
 };
 
+pub fn make_toydata(start: f32, end: f32, n: usize) -> Vec<ToyItem> {
+    let x = Array::<f32, Ix1>::linspace(start, end, n);
+
+    let normal = Normal::<f32>::new(0.0, 1.0).unwrap();
+    let mut rng = rand::rngs::StdRng::seed_from_u64(21);
+    let y = x.mapv(|x| x + x.sin() + normal.sample(&mut rng));
+
+    let toy_items = x
+        .into_iter()
+        .zip(y.into_iter())
+        .map(|(x, y)| ToyItem { x, y })
+        .collect();
+
+    toy_items
+}
+
+#[derive(Clone, Debug)]
+pub struct ToyItem {
+    pub x: f32,
+    pub y: f32,
+}
+
+pub struct ToyDataset {
+    dataset: Vec<ToyItem>,
+}
+
+impl Dataset<ToyItem> for ToyDataset {
+    fn get(&self, index: usize) -> Option<ToyItem> {
+        self.dataset.get(index).cloned()
+    }
+
+    fn len(&self) -> usize {
+        self.dataset.len()
+    }
+}
+
+pub struct ToyDatasetConfig {
+    pub start: f32,
+    pub end: f32,
+    pub n: usize,
+    pub split: f32,
+}
+
+impl ToyDatasetConfig {
+    pub fn train(&self) -> ToyDataset {
+        self.new("train")
+    }
+    pub fn test(&self) -> ToyDataset {
+        self.new("test")
+    }
+
+    fn new(&self, split: &str) -> ToyDataset {
+        let toy_items = make_toydata(self.start, self.end, self.n);
+
+        let amount = (self.split * self.n as f32).ceil() as usize;
+        let mut rng = rand::rngs::StdRng::seed_from_u64(12);
+
+        let items = match split {
+            "train" => toy_items.into_iter().choose_multiple(&mut rng, amount),
+            _ => toy_items
+                .into_iter()
+                .choose_multiple(&mut rng, self.n)
+                .drain(0..amount)
+                .collect(),
+        };
+
+        ToyDataset { dataset: items }
+    }
+}
+
 #[derive(Clone)]
 pub struct ToyBatcher<B: Backend> {
     device: B::Device,
@@ -26,12 +96,6 @@ impl<B: Backend> ToyBatcher<B> {
 pub struct ToyBatch<B: Backend> {
     pub x: Tensor<B, 2>,
     pub y: Tensor<B, 2>,
-}
-
-#[derive(Clone, Debug)]
-pub struct ToyItem {
-    pub x: f32,
-    pub y: f32,
 }
 
 impl<B: Backend> Batcher<ToyItem, ToyBatch<B>> for ToyBatcher<B> {
@@ -55,57 +119,5 @@ impl<B: Backend> Batcher<ToyItem, ToyBatch<B>> for ToyBatcher<B> {
         let y = Tensor::cat(y, 0).to_device(&self.device);
 
         ToyBatch { x, y }
-    }
-}
-
-pub struct ToyDataset {
-    dataset: Vec<ToyItem>,
-}
-
-impl Dataset<ToyItem> for ToyDataset {
-    fn get(&self, index: usize) -> Option<ToyItem> {
-        self.dataset.get(index).cloned()
-    }
-
-    fn len(&self) -> usize {
-        self.dataset.len()
-    }
-}
-
-impl ToyDataset {
-    const START: f32 = 0.0;
-    const END: f32 = 20.0;
-    const N: usize = 5000;
-    const SPLIT: f32 = 0.8;
-
-    pub fn train() -> Self {
-        Self::new("train")
-    }
-    pub fn test() -> Self {
-        Self::new("test")
-    }
-
-    fn new(split: &str) -> Self {
-        let mut rng = rand::rngs::StdRng::seed_from_u64(12);
-        let x = Array::<f32, Ix1>::linspace(Self::START, Self::END, Self::N);
-        let normal = Normal::<f32>::new(0.0, 1.0).unwrap();
-
-        let y = x.mapv(|x| x + x.sin() + normal.sample(&mut rng));
-        let toy_items = x
-            .into_iter()
-            .zip(y.into_iter())
-            .map(|(x, y)| ToyItem { x, y });
-
-        let amount = (Self::SPLIT * Self::N as f32).ceil() as usize;
-
-        let items = match split {
-            "train" => toy_items.choose_multiple(&mut rng, amount),
-            _ => toy_items
-                .choose_multiple(&mut rng, Self::N)
-                .drain(0..amount)
-                .collect(),
-        };
-
-        Self { dataset: items }
     }
 }
